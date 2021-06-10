f60ffbcbd678a4dd6c604f4ffe7c0bc153937f8b
Vishnu <vishnuvichu9487@gmail.com>
2021-06-10 13:44:19 +0530

    changes are updated

--- calendar_new/calendar-new.js
+++ calendar_new/calendar-new.js
@@ -54,9 +54,11 @@ function month_dis(){
    var yr = 2021;
 
    var dat = 1;
-     var day1,div,p,txt,res;
+
+    var day1,div,p,txt,res;
      
-     for(var i = 0; i < day.length; i++){
+    for(var i = 0; i < day.length; i++)
+    {
 
         day1 = day[i];
 
@@ -101,35 +103,74 @@ function month_dis(){
 
         var f_day = 0;
 
-        var a = document.getElementById("mnt-s").value - 1;
+        var a = document.getElementById("mnt-s").value;
 
-     var txt1,div1,p1,res1;
+        var c = a - 1;
 
-     for(var k = 0; k <= a; k++){
-         f_day += mnth1[k];
-     }
+        var txt1,div1,p1,res1;
 
-     var find_d = (f_day - 1) % 7;
+   
 
-     if((find_d === 0) || (find_d === 1) || (find_d === 2) || (find_d === 3))
+     for(var k = 0; k < a; k++)
      {
-         var l = find_d + 3;
+        f_day += mnth1[k];
+        
+     }
+   
+     if( a == 1){
+
+      // alert("gtt");
+        var g = (((f_day - 1) % 7) + 3);
+      // alert(g);   
+        var h = day[g];
+      // alert(h);
      }
 
-     else if((find_d === 4) || (find_d === 5) || (find_d === 6))
-     {
-        var l = find_d - 4;
+
+
+     
+
+     else if(a == 2){
+      //   alert("guu");
+        var g =((f_day - 1) % 7) - 1;
+      //   alert(g); 
+        var h = day[g];
+      //   alert(h);
+     }
+
+     else if(a == 3 ||  a == 8) {
+        var g = ((f_day - 1) % 7) - 4;
+      //   alert(g);
+        var h = day[g];
+      //   alert(h);
+     }
+
+     else if(a == 4){
+      var g = ((f_day - 1) % 7) + 4;
+      // alert(g);
+      var h = day[g];
+      // alert(h);
+     }
+
+     else if(a == 5 || a == 7 || a == 10 || a == 12){
+      var g = ((f_day - 1) % 7) + 3;
+      // alert(g);
+      var h = day[g];
+      // alert(h);
+     }
+
+     else if(a == 6 || a == 9 || a == 11){
+      var g = ((f_day - 1) % 7) - 3;
+      // alert(g);
+      var h = day[g];
+      // alert(h);
      }
 
 
+     document.getElementById("mnth-name").innerHTML = mnth[c];
+     document.getElementById("mnth-name1").innerHTML = mnth[c];
 
-    
-
-
-     document.getElementById("mnth-name").innerHTML = mnth[a];
-     document.getElementById("mnth-name1").innerHTML = mnth[a];
-
-     for (var j = 1; j <= mnth1[a]; j++)
+     for (var j = 1; j <= mnth1[c]; j++)
      {
          
         txt1 = document.createTextNode(j);
@@ -140,10 +181,14 @@ function month_dis(){
         res1 = div1.appendChild(p1);
 
 
-        if(j === 1)
+
+        if(j == 1) 
         {
-            res1.style.gridColumnStart = l + 1;
+            res1.style.gridColumnStart = g + 1;
         }
+
+       
+       
 
         document.querySelector(".mnt-order1").appendChild(res1);
 
